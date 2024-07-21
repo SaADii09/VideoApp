694172d98006e138f2edffa81611dd9760582fc0
SaADii09 <saadahmad6830879@gmail.com>
2024-07-21 19:34:33 +0500

    register controller : done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,27 +5,36 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    const { fullname, email, username, password } = req.body;
+    const { fullName, email, username, password } = req.body;
     console.log("email: ", email);
     //beginners methods not any issue
-    //  if (fullname == "") {
-    //     throw new ApiError(400, "fullname is required");
+    //  if (fullName == "") {
+    //     throw new ApiError(400, "fullName is required");
     // }
 
     if (
-        [fullname, email, username, password].some(
+        [fullName, email, username, password].some(
             (field) => field?.trim() == ""
         )
     ) {
         throw new ApiError(400, "All fields are required");
     }
 
-    const existedUser = User.findOne({ $or: [{ username }, { email }] });
+    const existedUser = await User.findOne({ $or: [{ username }, { email }] });
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exits");
     }
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverIamgeLocalPath = req.files?.coverIamge[0]?.path;
+    // const coverIamgeLocalPath = req.files?.coverImage[0]?.path;
+    let coverIamgeLocalPath;
+    if (
+        req.files &&
+        Array.isArray(req.files.coverImage) &&
+        req.files.coverImage.length > 0
+    ) {
+        coverIamgeLocalPath = req.files.coverImage[0].path;
+    }
+
     if (!avatarLocalPath) {
         throw new ApiError(400, "avatar image is required");
     }
@@ -37,7 +46,7 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const user = await User.create({
-        fullname,
+        fullName,
         avatar: avatar.url,
         coverImage: coverImage?.url || "",
         email,
