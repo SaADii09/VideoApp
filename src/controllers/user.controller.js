0b6b2506a9f326ffcdb5f390585c4f3b6477139a
SaADii09 <saadahmad6830879@gmail.com>
2024-07-22 14:21:14 +0500

    feat: Standardize API response format
    Replace custom response formatting with the `ApiResponse` class, ensuring consistency in all API responses.
    
    feat: Standardize API responses
    
    Refactor controllers to use the `ApiResponse` class, ensuring consistent response structure across all API endpoints. This improves code readability and maintainability, making it easier to handle responses in both the frontend and backend.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -225,16 +225,18 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (res, res) => {
     return res
         .status(200)
-        .json(200, req.user, "current user fetched successfully")
+        .json(
+            new ApiResponse(200, req.user, "current user fetched successfully")
+        )
 })
 
-const UpdateAccountDetails = asyncHandler(async (req, res) => {
+const UpdateAccountDetails = asyncHandler(async (req, res) => { 
     const { fullName, email } = req.body
 
     if (!fullName || !email) {
         throw new ApiError(400, "All fields are required")
     }
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
